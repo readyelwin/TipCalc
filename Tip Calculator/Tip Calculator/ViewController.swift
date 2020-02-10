9bf8a733b00f80d1fcdf740738aa8d6fabad9cd9
readyelwin <readyelwin@gmail.com>
2020-02-10 17:55:16 -0500

    Add files via upload

---
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Saraah Cooper on 1/29/20.
+//  Copyright © 2020 Saraah Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
